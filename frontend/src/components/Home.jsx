801ec7b1517ef3ac9834be7f4fabd5cffa437b13
parikotisai <parikotisai@gmail.com>
2025-08-23 21:50:27 +0530

    testing1

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1>Welcome to Dummy LMS Project</h1>
-        <p>This is the home page of your Learning Management System.</p>
+        <p>This is the home page of your Learning Management System.....</p>
     </div>
 );
 
